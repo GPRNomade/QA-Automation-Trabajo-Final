ab0195411f56dedf7e9ae6dda8d20babd81ff7ed
GPRNomade <guadapadinrojas@gmail.com>
2024-09-02 00:53:50 -0300

    add login and cart validations

--- cypress/e2e/myTestSaucedemo.cy.js
+++ cypress/e2e/myTestSaucedemo.cy.js
@@ -18,26 +18,45 @@ describe('Usuario Standard', {testIsolation:false}, () => {
     }
   })
 
-  it('Agregar un producto al carrito',() =>{
-    // cy.get('.pricebar').each((button) => {
-    //   cy.wrap(button).find('button').contains('Add to cart').click()
-    // })
-    let getTitle = ""
-    let productsAdded = []
-    cy.get('button').contains('Add to cart').click()
-    cy.get('.inventory_item_name').then((element) => {
-      getTitle = element.text()
-      cy.log(getTitle)
-    })
-    productsAdded.push({
-      'title': getTitle,
-      'description':"",
-      'price':""
-    })
-    cy.log(productsAdded[0].title)
 
+
+
+  it('Agregar un producto al carrito',()=>{
    
-    let cartElements = []
+    cy.fixture('prodList.json').then((prodList) => {
+      let dataItem0 = Object.assign({}, {'name': prodList.item_0.name,
+        'description': prodList.item_0.description,
+        'price': prodList.item_0.price});
+      let dataItem1 = Object.assign({}, {'name': prodList.item_1.name,
+        'description':prodList.item_1.description,
+        'price':prodList.item_1.price})
+      let dataItem2 = Object.assign({},{'name': prodList.item_2.name,
+        'description':prodList.item_2.description,
+        'price':prodList.item_2.price})
+      let dataItem3 = Object.assign({},{'name': prodList.item_3.name,
+        'description':prodList.item_3.description,
+        'price':prodList.item_3.price})
+      let dataItem4 = Object.assign({},{'name': prodList.item_4.name,
+        'description':prodList.item_4.description,
+        'price':prodList.item_4.price})
+      let dataItem5 = Object.assign({},{'name': prodList.item_5.name,
+        'description':prodList.item_5.description,
+        'price':prodList.item_5.price})
+      cy.wrap(dataItem0).as('dataItem0')
+      cy.wrap(dataItem1).as('dataItem1')
+      cy.wrap(dataItem2).as('dataItem2')
+      cy.wrap(dataItem3).as('dataItem3')
+      cy.wrap(dataItem4).as('dataItem4')
+      cy.wrap(dataItem5).as('dataItem5')
+      
+      })
+      
+    
+
+    cy.get('.pricebar').each((button) => {
+      cy.wrap(button).find('button').contains('Add to cart').click()
+    })
+   
     cy.getAllLocalStorage().then((localStorageContent) => {
       try {
         const cartContents = JSON.parse(localStorageContent['https://www.saucedemo.com']['cart-contents']) //con json.parse hago de un string, un array
@@ -50,24 +69,29 @@ describe('Usuario Standard', {testIsolation:false}, () => {
         cy.log("El carrito tiene tiene una cantidad distinta de items agregados")
       }
     })
-    cy.log(cartElements)
+    
+
+    cy.get('.shopping_cart_link').click()
+    cy.get('.header_secondary_container span').contains('Your Cart').should('be.visible')
+
+     
+//validacion items carrito
+    cy.get('@dataItem0').then((dataItem0)=>{
+      cy.get('#item_0_title_link .inventory_item_name').contains(dataItem0.name).should('have.text', dataItem0.name);
+      cy.get('#item_0_title_link + .inventory_item_desc').contains(dataItem0.description).should('have.text', dataItem0.description);
+      cy.get('#item_0_title_link ~ .item_pricebar>.inventory_item_price').contains(dataItem0.price).should('have.text', dataItem0.price);
+    })
+    
   
 
-  
+    cy.get('#react-burger-menu-btn').click()
+    cy.get('#reset_sidebar_link').click()
     cy.clearAllLocalStorage();
 
   })
 
 
 
-
+// cuando haga las validaciones del plp: cy.get('#item_0_title_link inventory_item_img').should('have.attr', 'src').should('not.have.text', '404')
 
 })
-
-
-
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://www.saucedemo.com/')
-//   })
-// })
